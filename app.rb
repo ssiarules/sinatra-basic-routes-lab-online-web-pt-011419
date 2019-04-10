ee06a3094fcfdd8fa7d9bcb94f33598051da2783
Johnson Joseph <jhonsonj88@gmail.com>
2019-04-10 22:40:03 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get'/name' do  
+    "My name is Johnson Joseph"
+  end 
+  
+  get'/hometown' do 
+    "My hometown is Irvington, NJ"
+  end 
+  
+  get'/favorite-song' do 
+    "My favorite song is Dedication - Nipsey Hussle feat. Kendrick Lamar"
+  end 
+  
+  
 end
+
+
+
